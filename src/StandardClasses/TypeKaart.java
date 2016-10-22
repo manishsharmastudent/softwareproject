359755bcbc8fb0c84b0054130590f24653babd68
Rik Van Belle <Rik Van Belle@DESKTOP-S3TV984>
2016-10-22 21:28:45 +0200

    Toevoegen datamembers en methodes van klasse Korting en TypeKaart

---
+++ src/StandardClasses/TypeKaart.java
@@ -0,0 +1,34 @@
+package StandardClasses;
+
+public class TypeKaart {
+	private String naam;
+	private String omschrijving;
+	private Korting korting;
+
+	public String getNaam()
+	{
+		return this.naam;
+	}
+	public String getOmschrijving()
+	{
+		return this.omschrijving;
+	}
+	public Korting getKorting(){
+		return korting;
+	}
+	public boolean setNaam(String naam)
+	{
+		this.naam = naam;
+		return true;
+	}
+	public boolean setOmschrijving(String omschrijving)
+	{
+		this.omschrijving = omschrijving;
+		return true;
+	}
+	public boolean setKorting(Korting korting)
+	{
+		this.korting = korting;
+		return true;
+	}
+}
