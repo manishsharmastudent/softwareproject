e3dfb2e56db7d5e9564be7cff53ae97f50c94654
manishsharma <manish.sharma@student.ehb.be>
2016-11-10 15:13:30 +0100

    ticketview bezig

--- src/view/TicketView.java
+++ src/view/TicketView.java
@@ -1,7 +1,23 @@
 package view;
+import javax.swing.*;
 
 /**
  * Created by User on 31/10/2016.
  */
 public class TicketView {
+    private JFrame frame = new JFrame();
+    private JLabel welkomLabel = new JLabel("Welkom");
+    private JLabel tijdLabel = new JLabel("tijd");
+    private JLabel datumLabel = new JLabel("datum");
+    private JPanel welkomPanel = new JPanel();
+    private JPanel panel = new JPanel();
+    private JPanel interactiePanel = new JPanel();
+    private JLabel vertrekLabel = new JLabel("Vertrekpunt");
+    private JLabel aankomstLabel = new JLabel("Aankomst");
+    private JLabel datumticketLabel = new JLabel("Datum");
+    private JLabel aantalLabel = new JLabel("Aantal");
+    private JLabel klasseLabel = new JLabel("Klasse");
+    private JButton terugButton = new JButton("Terug");
+    private JButton zoekButton = new JButton("Zoek");
+
 }
