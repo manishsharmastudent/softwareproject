7e2c5c72ee58bcc8e9556e83f97e7eb4f9c4e4df
manishsharma <manish.sharma@student.ehb.be>
2016-11-12 22:21:37 +0100

    ticketview bijna af

--- src/view/TicketView.java
+++ src/view/TicketView.java
@@ -13,6 +13,9 @@ import java.text.SimpleDateFormat;
 import java.util.Properties;
 import java.util.Calendar;
 
+
+
+
 /**
  * Created by User on 31/10/2016.
  */
@@ -23,7 +26,7 @@ public class TicketView {
     private JLabel datumLabel = new JLabel("datum");
     private JPanel welkomPanel = new JPanel();
     private JPanel panel = new JPanel();
-    private JPanel interactiePanel = new JPanel();
+    private JPanel interactiePanel = new JPanel(new SpringLayout());
     private JLabel vertrekLabel = new JLabel("Vertrekpunt");
     private JLabel aankomstLabel = new JLabel("Aankomst");
     private JLabel datumticketLabel = new JLabel("Datum");
@@ -34,7 +37,18 @@ public class TicketView {
     private AutoCompleteDecorator decorator;
     private JComboBox stationCombobox;
     private JComboBox stationTweeCombobox;
+    private JComboBox klasseCombobox;
     private Properties p = new Properties();
+    private Integer value = new Integer(1);
+    private Integer min = new Integer(1);
+    private Integer max = new Integer(100);
+    private Integer step = new Integer(1);
+    private SpinnerNumberModel modelSp = new SpinnerNumberModel(value, min, max, step);
+    private int fifty = modelSp.getNumber().intValue();
+    private JSpinner spinner1 = new JSpinner(modelSp);
+    private JPanel mainPanel = new JPanel();
+    private JPanel navigationPanel = new JPanel();
+
     private UtilDateModel model = new UtilDateModel();
     private JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
     private JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new JFormattedTextField.AbstractFormatter() {
@@ -66,17 +80,27 @@ public class TicketView {
 
     private void setGui(){
         stationCombobox = new JComboBox(new Object[]{"","test", "aardappeltest"});
+        stationTweeCombobox = new JComboBox(new Object[]{"","test", "aardappeltest"});
+        klasseCombobox = new JComboBox(new Object[]{"","Eerste klasse", "Tweede klasse"});
+
+
+
         AutoCompleteDecorator.decorate(stationCombobox);
+        AutoCompleteDecorator.decorate(stationTweeCombobox);
+
+        frame.setSize(800,700);
 
 
-        frame.add(panel);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
 
 
 
 
 
 
-        panel.setLayout(new BorderLayout(20,20));
+
 
 
 
@@ -90,13 +114,57 @@ public class TicketView {
 
 
 
+        interactiePanel.setLayout(null);
+        vertrekLabel.setBounds(30, 180, 180, 25);
+        stationCombobox.setBounds(200, 180, 180, 25);
+        aankomstLabel.setBounds(30, 220, 180, 25);
+        stationTweeCombobox.setBounds(200, 220, 180, 25);
+        datePicker.setBounds(200, 260, 180, 25);
+        datumticketLabel.setBounds(30, 260, 180, 25);
+        aantalLabel.setBounds(30, 300, 180, 25);
+        klasseLabel.setBounds(30, 340, 180, 25);
+        klasseCombobox.setBounds(200, 340, 180, 25);
+        spinner1.setBounds(200, 300, 180, 25);
+        zoekButton.setBounds(330, 400, 180, 25);
+        terugButton.setBounds(600, 20, 90, 40);
 
-
+        interactiePanel.add(stationTweeCombobox);
+        interactiePanel.add(vertrekLabel);
+        interactiePanel.add(stationCombobox);
+        interactiePanel.add(aankomstLabel);
+        interactiePanel.add(datumticketLabel);
         interactiePanel.add(datePicker);
+        interactiePanel.add(aantalLabel);
+        interactiePanel.add(datePicker);
+        interactiePanel.add(klasseLabel);
+        interactiePanel.add(klasseCombobox);
+        interactiePanel.add(spinner1);
+        interactiePanel.add(zoekButton);
+        interactiePanel.setBorder(border);
+
+
+
 
         welkomPanel.add(welkomLabel);
+        navigationPanel.setLayout(new BorderLayout(0,0));
+        navigationPanel.add(terugButton, BorderLayout.EAST);
+        navigationPanel.setBorder(border);
+
+
+
+
+
+        panel.setLayout(new BorderLayout(80,30));
+
+        mainPanel.setLayout(new BorderLayout(0,0));
+        mainPanel.add(navigationPanel, BorderLayout.NORTH);
+        mainPanel.add(interactiePanel, BorderLayout.CENTER);
+
+
+
         panel.add(welkomPanel, BorderLayout.NORTH);
-        panel.add(interactiePanel, BorderLayout.CENTER);
+        panel.add(mainPanel, BorderLayout.CENTER);
+
 
 
 
@@ -104,15 +172,9 @@ public class TicketView {
         p.put("text.month", "Month");
         p.put("text.year", "Year");
 
-        frame.setSize(800,700);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-
-
-        frame.setLocationRelativeTo(null);
-
-        frame.setVisible(true);
+        frame.add(panel);
 
 
 
