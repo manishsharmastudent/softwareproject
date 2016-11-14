32eafdd0669ed6cd78d3beed2858ccc23a4a570f
manishstudent <manish.sharma@student.ehb.be>
2016-11-14 17:51:27 +0100

    Merge remote-tracking branch 'remotes/origin/Manish' into Rik
    # Conflicts:
    #	.idea/workspace.xml
    #	src/Main.java
    #	src/controller/VoorwerpController.java
    
    Loginscreen afgewerkt
    HomeView gemaakt
    Login inorde gebracht
    Login gekoppeld aan homeView
    TicketView en HomeView afgemaakt
    TicketView gekoppelt aan homeview

--- src/Main.java
+++ src/Main.java
@@ -16,21 +16,6 @@ public class Main {
 
     public static void main(String[] args) {
         SessionFactory factory = SessionFactorySingleton.getInstance().getSessionFactory();
-        /*ManageKlant mK = new ManageKlant();
-        Klant k1 = new Klant("123-123", "Rik", "Van Belle", "Vrijheidstraat 25", 9400, "Ninove", true);
-        KlantView KView = new KlantView("Klant");
-        KlantController KC = new KlantController(k1, KView, mK);
-
-
-        ManageVoorwerp MV = new ManageVoorwerp();
-        Station stationVertrek = new Station(1,"Brussel-Zuid","Brussel", true);
-        Station stationAankomst = new Station(1,"Vilvoorde","Vilvoorde", true);
-        Route route = new Route(1,stationVertrek,stationAankomst, true);
-    //  Voorwerp v = new Voorwerp(1,1345, stationAankomst,route,"Kous", "Geel",k1,true);
-        Voorwerp v1 = new Voorwerp(2,1,"Groen","elektronica","GSM", route, stationAankomst,k1,true);
-        VoorwerpView voorwerpView = new VoorwerpView("Voorwerp");
-        VoorwerpController voorwerpController = new VoorwerpController(v1, voorwerpView);
-        voorwerpController.showVoorwerpen();*/
 
         LoginController lC = new LoginController();
         lC.showLoginScreen();
