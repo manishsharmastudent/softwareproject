f6eabcb19bac3e9cf351f8db699da64808440574
manishstudent <manish.sharma@student.ehb.be>
2016-11-21 18:42:59 +0100

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
 import controller.*;
-import hibernate.*;
-import view.*;
-import model.*;
-
-import org.hibernate.SessionFactory;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by Manish on 10/10/2016.
  */
 public class Main {
-
     public static void main(String[] args) {
         LoginController loginController = new LoginController();
         loginController.showLoginScreen();
