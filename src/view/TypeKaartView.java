08f42d59b71ce7c3b3f6363617ca9a0fbd041eb1
Thequalizer <dietger.pieters@student.ehb.be>
2016-12-12 10:04:42 +0100

    typekaartview af

--- src/view/TypeKaartView.java
+++ src/view/TypeKaartView.java
@@ -19,8 +19,7 @@ public class TypeKaartView extends StandardView {
 
     private JButton doorgaanButton = new JButton("Doorgaan");
     private JButton doorgaanTweedeButton = new JButton("Doorgaan");
-    private JTextField prijsText = new JTextField("€0,00");
-    private JLabel prijsLabel = new JLabel("Prijs: ");
+
     private JPanel doorgaanPanel = new JPanel();
     private JPanel mainInteractiePanel = new JPanel();
     private JPanel prijsPanel = new JPanel();
@@ -33,17 +32,18 @@ public class TypeKaartView extends StandardView {
 
 
     public void showTicketten(){
+        this.showWindow();
         int aantalTickets = 15;
+        interactiePanel.setLayout(new BoxLayout(interactiePanel, BoxLayout.PAGE_AXIS));
         mainInteractiePanel.setLayout(new BoxLayout(mainInteractiePanel, BoxLayout.PAGE_AXIS));
         doorgaanPanel.setLayout(new BoxLayout(doorgaanPanel, BoxLayout.PAGE_AXIS));
-        prijsPanel.setLayout(null);
+
 
 
         doorgaanButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         doorgaanTweedeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 
-        mainInteractiePanel.setBackground(Color.CYAN);
 
 
 
@@ -59,22 +59,16 @@ public class TypeKaartView extends StandardView {
         scrollPane.setMinimumSize(scrollPane.getPreferredSize());
 
 
-        prijsLabel.setBounds(290,110,100,50);
-        prijsText.setBounds(330,120,150,30);
 
-        prijsPanel.add(prijsLabel);
-        prijsPanel.add(prijsText);
 
 
 
         doorgaanPanel.add(prijsPanel);
         doorgaanPanel.add(doorgaanTweedeButton);
 
-        doorgaanPanel.setBackground(Color.red);
 
         for (int i =0; i < aantalTickets; i++){
             JPanel ticketPanel = new JPanel(new GridLayout(0,9));
-
 
 
             ticketPanel.setPreferredSize(new Dimension((mainPanel.getWidth() - 100), 50));
@@ -82,14 +76,14 @@ public class TypeKaartView extends StandardView {
             ticketPanel.setMinimumSize(ticketPanel.getPreferredSize());
 
             JCheckBox checkTicketBox = new JCheckBox();
-            JLabel stationLabel = new JLabel("Station");
+            JLabel stationLabel = new JLabel("Staion");
             JLabel aankomstStationLabel = new JLabel("Station");
             JLabel arrowLabel = new JLabel();
             String path = "/resources/images/arrow.png";
             java.net.URL imgURL = getClass().getResource(path);
-            JLabel datunLabel = new JLabel("datun");
+            JLabel datunLabel = new JLabel("datum");
             JLabel duurLabel = new JLabel("duur");
-            JLabel typeTreinLabel = new JLabel("(IC,P,..)");
+            JLabel typeTreinLabel = new JLabel("(ICn,P,..)");
             JLabel overstapLabel = new JLabel("wissels: " + i);
 
 
@@ -106,13 +100,13 @@ public class TypeKaartView extends StandardView {
 
 
 
-
-                arrowLabel.setIcon(icon);
+//
+                arrowLabel.setIcon(icon);//knjj
 
                 ticketPanel.add(arrowLabel);
 
             } else {
-                System.err.println("Couldn't find file: " + path);
+                System.err.println("Couldnn't find file: " + path);
             }
 
 
@@ -136,12 +130,12 @@ public class TypeKaartView extends StandardView {
 
 
         interactiePanel.add(scrollPane);
-        mainInteractiePanel.add(doorgaanButton);
 
+        interactiePanel.add(doorgaanButton);
+        doorgaanPanel.setBorder(border);
 
         interactiePanel.add(doorgaanPanel);
 
-        doorgaanPanel.setBorder(border);
 
 
 
@@ -163,10 +157,7 @@ public class TypeKaartView extends StandardView {
     public static void main(String[] args) {
 
         TypeKaartView t = new TypeKaartView("iets");
-        t.initMenuBar();
-        t.initNavTree();
-        t.showWindow();
-        t.showPath();
+
         t.showTicketten();
     }
 
