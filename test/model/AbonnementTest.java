c52ea1c67f9b4451916cffab5db07ef766faad47
nuttwerx <nofel.tiani@gmail.com>
2016-12-25 16:51:41 +0100

    opstellen testenv

---
+++ test/model/AbonnementTest.java
@@ -0,0 +1,97 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by NT on 25-12-16.
+ */
+public class AbonnementTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getAbonnementId() throws Exception {
+
+    }
+
+    @Test
+    public void getKorting() throws Exception {
+
+    }
+
+    @Test
+    public void getBeginDatum() throws Exception {
+
+    }
+
+    @Test
+    public void getVervalDatum() throws Exception {
+
+    }
+
+    @Test
+    public void getRoute() throws Exception {
+
+    }
+
+    @Test
+    public void getKlant() throws Exception {
+
+    }
+
+    @Test
+    public void getPrijs() throws Exception {
+
+    }
+
+    @Test
+    public void isActive() throws Exception {
+
+    }
+
+    @Test
+    public void setAbonnementId() throws Exception {
+
+    }
+
+    @Test
+    public void setKorting() throws Exception {
+
+    }
+
+    @Test
+    public void setBeginDatum() throws Exception {
+
+    }
+
+    @Test
+    public void setVervalDatum() throws Exception {
+
+    }
+
+    @Test
+    public void setRoute() throws Exception {
+
+    }
+
+    @Test
+    public void setKlant() throws Exception {
+
+    }
+
+    @Test
+    public void setPrijs() throws Exception {
+
+    }
+
+    @Test
+    public void setActive() throws Exception {
+
+    }
+
+}
