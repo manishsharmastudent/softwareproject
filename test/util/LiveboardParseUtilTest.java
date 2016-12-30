af830504e1fc19e3604af2c74e588f568f03b3e7
nuttwerx <nofel.tiani@gmail.com>
2016-12-30 13:05:31 +0100

    schrijven test liveboardparseutil

---
+++ test/util/LiveboardParseUtilTest.java
@@ -0,0 +1,49 @@
+package util;
+
+import model.Liveboard;
+import model.Station;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nofel on 30-12-16.
+ */
+public class LiveboardParseUtilTest {
+    Map<String, Liveboard> liveboardMap;
+    Station station;
+    Liveboard liveboard;
+
+    @Before
+    public void setUp() throws Exception {
+        station = new Station();
+    }
+
+    @Test
+    public void writeLiveboardToCache() throws Exception {
+        for(int i = 1; i < 11; i++){
+            station.setNaam("Station " + i);
+            if(!(LiveboardParseUtil.writeLiveboardToCache(new Liveboard(station, LocalDateTime.now()))))
+                fail();
+        }
+    }
+
+    @Test
+    public void getLiveboardFromCacheHashMap() throws Exception {
+        liveboardMap = LiveboardParseUtil.getLiveboardFromCache();
+        for(int i = 1; i<11;i++)
+            assertEquals("Station " + i, liveboardMap.get("Station " + i).getStation().getNaam());
+    }
+
+    @Test
+    public void getLiveboardFromCacheLiveboard() throws Exception {
+        liveboardMap = LiveboardParseUtil.getLiveboardFromCache();
+        liveboard = liveboardMap.get("Station 3");
+        assertEquals("Station 3", liveboard.getStation().getNaam());
+    }
+
+}
